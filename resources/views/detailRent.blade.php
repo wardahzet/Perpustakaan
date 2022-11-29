78c27b2be38d3c526fda32c969db7dc449850199
Siti Waheeda Najihah <sitiwaheeda14@student.ub.ac.id>
2022-11-29 16:50:51 +0700

    apa

---
+++ resources/views/detailRent.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+  <!--  This source code is exported from pxCode, you can get more document from https://www.pxcode.io  -->
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/aos@2.3.1/dist/aos.css" />
+    <!-- Please DO NOT remove this line, all link and script tags will be merged to here -->
+
+    <link rel="stylesheet" type="text/css" href="/css/common.css" />
+    <link rel="stylesheet" type="text/css" href="/css/fonts.css" />
+    <link rel="stylesheet" type="text/css" href="/css/tailwind.css" />
+
+    <script type="text/javascript" src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script type="text/javascript" src="https://unpkg.com/headroom.js@0.12.0/dist/headroom.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/px2code/posize/build/v1.01.0.js"></script>
+  </head>
+
+  <body class="flex-column">
+    <div class="halaman-detail flex flex-col bg-white relative overflow-hidden min-h-[1137px]">
+      /*This group structure is not ready for flow layout, please resolve the ❗alone items in pxCode editor.*/
+      <div
+        class="bg-[rgb(255,250,239)] rounded-[15px] w-[1300px] h-[82px] absolute top-[98px] left-[70px] min-w-[1300px]"
+      ></div>
+      <div
+        class="bg-[rgb(255,250,239)] rounded-[15px] w-[1300px] h-[371px] absolute top-[196px] left-[70px] min-w-[1300px]"
+      ></div>
+      <div
+        class="bg-[rgb(255,250,239)] rounded-[15px] w-[1300px] h-[371px] absolute top-[583px] left-[70px] min-w-[1300px]"
+      ></div>
+      <img
+        class="rounded-[15px] w-[180px] h-auto align-top object-contain object-[center_top] absolute top-[619px] left-[180px] min-w-[180px]"
+        src="/assets/3f14b777f518c5f0273e9b136385d2b6.png"
+        alt="alt text"
+      />
+      <h5
+        class="font-normal text-base font-Poppins text-black tracking-[0px] w-fit h-min absolute top-[351px] left-[991px] min-w-0"
+      >
+        Erlangga
+      </h5>
+      <h5
+        class="font-normal text-base font-Poppins text-black tracking-[0px] w-fit h-min absolute top-[351px] left-[1182px] min-w-0"
+      >
+        Fiction
+      </h5>
+      <h5
+        class="font-normal text-base font-Poppins text-black tracking-[0px] w-fit h-min absolute top-[752px] left-[1156px] min-w-0"
+      >
+        Encyclopedia
+      </h5>
+      <div
+        class="bg-[rgb(255,250,239)] outline outline-black outline-1 outline-offset-[-1px] w-[30px] h-[30px] absolute top-[126px] left-[102px] min-w-[30px]"
+      ></div>
+      <div
+        class="bg-[rgb(255,250,239)] outline outline-black outline-1 outline-offset-[-1px] w-[30px] h-[30px] absolute top-[366px] left-[102px] min-w-[30px]"
+      ></div>
+      <div
+        class="bg-[rgb(255,250,239)] outline outline-black outline-1 outline-offset-[-1px] w-[30px] h-[30px] absolute top-[751px] left-[102px] min-w-[30px]"
+      ></div>
+      <px-grid x="180fr minmax(0px, max-content) 1190fr" y="128px minmax(0px, max-content) 982fr" absolute="true"
+        ><h4
+          class="flex justify-center font-normal text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          Product
+        </h4></px-grid
+      ><px-grid x="820fr minmax(0px, max-content) 557fr" y="126px minmax(0px, max-content) 984fr" absolute="true"
+        ><h4
+          class="flex justify-center font-normal text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          Penulis
+        </h4></px-grid
+      ><px-grid x="991fr minmax(0px, max-content) 374fr" y="128px minmax(0px, max-content) 982fr" absolute="true"
+        ><h4
+          class="flex justify-center font-normal text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          Penerbit
+        </h4></px-grid
+      ><px-grid x="1161fr minmax(0px, max-content) 182fr" y="128px minmax(0px, max-content) 982fr" absolute="true"
+        ><h4
+          class="flex justify-center font-normal text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          Kategories
+        </h4></px-grid
+      ><px-grid x="648fr minmax(0px, max-content) 753fr" y="128px minmax(0px, max-content) 982fr" absolute="true"
+        ><h4
+          class="flex justify-center font-normal text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          ISBN
+        </h4></px-grid
+      ><img
+        class="rounded-[15px] w-[180px] h-auto align-top object-contain object-[center_top] absolute top-[231px] left-[180px] min-w-[180px]"
+        src="/assets/7cb73d44671c5ea52c11189581a32440.png"
+        alt="alt text"
+      /><px-grid x="400px 176fr 864fr" y="339px minmax(0px, max-content) 726fr" absolute="true"
+        ><h5 class="font-semibold text-base font-Poppins text-black tracking-[0px] w-full h-full">
+          Under The Kitchen Table - Dessy Miladeana
+        </h5></px-grid
+      ><px-grid x="400px 176fr 864fr" y="733px minmax(0px, max-content) 332fr" absolute="true"
+        ><h5 class="font-semibold text-base font-Poppins text-black tracking-[0px] w-full h-full">
+          Ensiklopedia Sains (Dilengkapi 1000 tautan internet)
+        </h5></px-grid
+      >
+      <h5
+        class="font-normal text-base font-Poppins text-black tracking-[0px] w-fit h-min absolute top-[749px] min-h-[48px] left-[609px] min-w-0"
+      >
+        9786022497301<br />
+      </h5>
+      <px-grid x="815px 67fr 558fr" y="749px minmax(0px, 24fr) 364fr" absolute="true"
+        ><h5 class="font-normal text-base font-Poppins text-black tracking-[0px] w-full h-full">
+          Usborne<br /></h5></px-grid
+      ><px-grid x="978px 105fr 357fr" y="749px minmax(0px, 48fr) 340fr" absolute="true"
+        ><h5 class="font-normal text-base font-Poppins text-black tracking-[0px] w-full h-full">
+          Bhuana Ilmu Populer<br /></h5
+      ></px-grid>
+      <div class="bg-[rgb(217,217,217)] w-[1440px] h-[107px] absolute top-[1030px] left-0 min-w-[1440px]"></div>
+      <h5
+        class="font-normal text-base font-Poppins text-black tracking-[0px] w-fit h-min absolute top-32 left-[1309px] min-w-0"
+      >
+        Aksi
+      </h5>
+      <img
+        class="w-5 h-auto align-top object-contain object-[center_top] absolute top-[753px] left-[1315px] min-w-[20px]"
+        src="/assets/4ed04611bedfc49cdb2d33826de55be8.png"
+        alt="alt text"
+      />
+      <div
+        class="bg-[rgb(234,153,1)] rounded-[20px] w-[310px] h-[70px] absolute top-[1049px] left-[1060px] min-w-[310px]"
+      ></div>
+      <img
+        class="w-5 h-auto align-top object-contain object-[center_top] absolute top-[352px] left-[1315px] min-w-[20px]"
+        src="/assets/4ed04611bedfc49cdb2d33826de55be8.png"
+        alt="alt text"
+      />
+      <h3
+        class="font-semibold text-[20px] leading-normal font-Poppins text-white tracking-[0px] w-fit h-min absolute top-[1069px] left-[1144px] min-w-0"
+      >
+        Check Out (-)
+      </h3>
+      <div
+        class="bg-[rgb(217,217,217)] rounded-[20px] w-[640px] h-[46px] absolute top-2.5 left-[290px] min-w-[640px]"
+      ></div>
+      <img
+        class="w-[200px] h-auto align-top object-contain object-[center_top] absolute top-[3px] left-[70px] min-w-[200px]"
+        src="/assets/b4124a2fa4037f0b25a117debced7de2.png"
+        alt="alt text"
+      />
+      <div
+        class="flex flex-col bg-[image:var(--src)] bg-[center_center] bg-cover bg-no-repeat w-5 h-5 absolute overflow-hidden top-[23px] left-[308px] min-w-[20px]"
+        style="--src:url(/assets/f41a63f1ffe660a7bd21d79a79deb908.png)"
+      ></div>
+      <px-grid x="971fr minmax(0px, max-content) 410fr" y="19px minmax(0px, max-content) 1091fr" absolute="true"
+        ><h4
+          class="flex justify-center font-semibold text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          Home
+        </h4></px-grid
+      ><px-grid x="1056fr minmax(0px, max-content) 312fr" y="19px minmax(0px, max-content) 1091fr" absolute="true"
+        ><h4
+          class="flex justify-center font-semibold text-[18px] leading-normal font-Poppins text-black text-center tracking-[0px] w-full h-full"
+        >
+          Wishlist
+        </h4></px-grid
+      >
+      <div
+        class="border-solid border-black border-[0px 0px 1px 0px] w-[1440px] h-[67px] absolute top-0 left-0 min-w-[1440px]"
+      ></div>
+      <div
+        class="bg-[rgb(217,217,217)] rounded-[20px] w-[640px] h-[46px] absolute top-2.5 left-[290px] min-w-[640px]"
+      ></div>
+      <img
+        class="w-[200px] h-auto align-top object-contain object-[center_top] absolute top-[3px] left-[70px] min-w-[200px]"
+        src="/assets/b4124a2fa4037f0b25a117debced7de2.png"
+        alt="alt text"
+      />
+      <div
+        class="flex flex-col bg-[image:var(--src)] bg-[center_center] bg-cover bg-no-repeat w-5 h-5 absolute overflow-hidden top-[23px] left-[308px] min-w-[20px]"
+        style="--src:url(/assets/f41a63f1ffe660a7bd21d79a79deb908.png)"
+      ></div>
+    </div>
+    <script type="text/javascript">
+      AOS.init();
+    </script>
+  </body>
+</html>

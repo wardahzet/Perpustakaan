735a42af52de61b5c19883062bb047c8484f753e
Alifia Putri Budiyanti <101036030+alifiaputri14@users.noreply.github.com>
2022-12-02 14:37:12 +0700

    Create review-peminjaman.blade.php

---
+++ resources/views/review-peminjaman.blade.php
@@ -0,0 +1,120 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <div class="relative border-solid border-b-2 border-gray-300 py-2">
+    <div class="container flex items-center mx-20  ">
+      
+        <img src="https://i.ibb.co/Q9vPH3J/Rectangle-119.png" alt="">
+
+        <input type="text" class=" block w-6/12 mx-10 h-10 px-4 py-2 text-base bg-gray-100 bg-clip-padding border rounded transition ease-in-out focus:text-gray-700 focus:bg-gray-100 focus:border-[#EA9901] focus:outline-none   input-search bg-white-200 border-solid border-2 border-gray-300 rounded-2xl  text-gray-700" placeholder="search" >
+        <a href="#" class="link-1 mx-5 font-semibold text-base  ">Home</a>
+        <a href="#" class="link-2 mx-5 font font-semibold text-base">Rent</a>
+      
+    </div>
+  </div>
+   <div class="profile px-10 my-5 ">
+     <div class="container bg bg-yellow-200 px-10 py-5 rounded-lg  ">
+       <div class="name flex py-5">
+         <p>Name</p>
+         <p class="ml-auto">Alifia Putri Budiyanti</p>
+       </div>
+       <div class="email flex py-5 ">
+         <p>Email</p>
+         <p class="ml-auto">aisyahputri140702@gmail.com</p>
+       </div>
+       <div class="no-anggota flex py-5">
+         <p>No Anggota</p>
+         <p class="ml-auto">315150700111012</p>
+       </div>
+       <div class="no-handphone flex py-5">
+         <p>No Handphone</p>
+         <p class="ml-auto">+62 896- 1686-871</p>
+       </div>
+     </div>
+   </div>
+    
+    <div class="profile px-10 my-5 ">
+     <div class="container bg bg-yellow-200 px-10 py-5 rounded-lg  ">
+       <div class="name flex py-5">
+         <p class="mr-80 ">Nama Product</p>
+         <p class="ml-auto ">ISBN</p>
+         <p class="ml-auto">Penulis</p>
+         <p class="ml-auto">Penerbit</p>
+         <p class="ml-auto">Kategori</p>
+       </div>
+       <div class="content-1 py-5 flex items-center">
+         <div class="image-1">
+           <img src="https://i.ibb.co/cXzh6hd/Rectangle-115.png" alt="">
+
+         </div>
+         <div class="ml-5">
+           <p>Under The Kitchen Table - Dessy <br> Miladeana</p>
+         </div>
+         <div class="ml-36">
+           <p>9786020664156</p>
+         </div>
+         <div class="ml-28">
+           <p>Dessy Miladeana</p>
+         </div>
+         <div class="ml-36">
+           <p>Erlangga</p>
+         </div>
+         <div class="ml-44">
+           <p>Fiction</p>
+         </div>
+       </div>
+        <div class="content-1 py-5 flex items-center">
+         <div class="image-1">
+           <img src="https://i.ibb.co/nm99syR/123321.png" alt="">
+
+         </div>
+         <div class="ml-5">
+           <p>Ensiklopedia Sains (Dilengkapi <br> 1000 tautan internet)</p>
+         </div>
+         <div class="ml-56">
+           <p>9786022497301</p>
+         </div>
+         <div class="ml-36">
+           <p>Usborne</p>
+         </div>
+         <div class="ml-40">
+           <p>Bhuana Ilmu <br> Populer</p>
+         </div>
+         <div class="ml-44">
+           <p>Encyclopedia</p>
+         </div>
+       </div>
+     </div>
+     
+   </div>
+    <div class="profile px-10 my-5 ">
+     <div class="container bg bg-yellow-200 px-10 py-5 rounded-lg  ">
+       <div class="name flex py-5">
+         <p>Nomor Peminjaman</p>
+         <p class="ml-auto">PIB-25112-AIIS</p>
+       </div>
+       <div class="email flex py-5 ">
+         <p>Tanggal Peminjaman</p>
+         <p class="ml-auto">12-November-2022</p>
+       </div>
+       <div class="no-anggota flex py-5">
+         <p>Tanggal Pengembalian </p>
+         <p class="ml-auto">29-November-2022</p>
+       </div>
+       
+     </div>
+     
+   </div>
+   <div class="bg-[#D9D9D9] flex px-16 py-2">
+      <div class="button ml-auto">
+        <button type="submit" class="bg-gray-500 px-10 py-3 rounded-xl  bg-[#EA9901] focus:border-solid focus:bg-white focus:border-[#EA9901] focus:border-2" >Check Out</button>
+      </div>
+    </div>
+</body>
+</html>

f45b6e2d8a2e43ab089060b472c5041dbc84b14c
auhppy <auhappycahya11@gmail.com>
2022-12-02 12:14:16 +0700

    Create bookDetails.blade.php

---
+++ resources/views/bookDetails.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Details</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="relative bg-white">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6">
+            <div class="flex items-center justify-between border-b-2 border-gray-300 py-6 md:justify-start md:space-x-10">
+                <div class="flex justify-start lg:w-0 lg:flex-1">
+                    <a href="#">
+                    <span class="sr-only">E-Library</span>
+                    <img class="h-8 w-auto sm:h-10" src="https://i.ibb.co/XYThtt1/Logo-ELibrary.png" alt="">
+                    </a>
+                </div>
+            <div class="flex justify-end">
+                <div class="mb-0 xl:w-96 mr-16 -mx-60">
+                    <input
+                    type="search"
+                    class=" form-control block w-full px-4 py-2 text-base text-gray-700 bg-gray-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-gray-100 focus:border-[#EA9901] focus:outline-none hover:border-[#EA9901] 
+                    id="exampleSearch" 
+                    placeholder="Search"
+                    />
+                </div>
+            </div>
+            <nav class="hidden space-x-10 md:flex">
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Rent</a>
+            </nav>
+            <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                <a href="#" class="inline-flex items-center rounded-md border border-[#EA9901] bg-white px-4 py-2 text-base font-medium text-[#EA9901] shadow-sm hover:bg-gray-300">Register</a>
+                <a href="#" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-[#EA9901] -600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-[#BF7D01]">Login</a>
+            </div>
+        </div>
+    </div>
+
+<div class="bg-white">
+    <div class="pt-6">
+      <nav aria-label="Breadcrumb">
+        <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <li>
+            <div class="flex items-center">
+              <a href="#" class="mr-2 text-sm font-medium text-gray-900">Book Details</a>
+            </div>
+          </li>
+      </nav>
+  
+      <!-- Cover Image -->
+      <div class="mx-auto mt-6 max-w-2xl sm:px-6 px-4 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+        <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+          <div class="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg ">
+            <img src="https://i.ibb.co/r2Zxg25/image001-118.jpg" alt="Cover" class="h-72 object-cover object-center rounded-lg">
+          </div>
+        </div>
+      </div>
+  
+      <div class="pl-16 px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:pt-16">
+        <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+          <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Teluk Alaska</h1>
+        </div>
+  
+        <div class="mt-4 lg:row-span-3 lg:mt-0">
+          <p class="text-3xl tracking-tight text-gray-900">You May Also Like</p>
+          <form class="mt-10">
+            <div>
+              <h3 class="text-lg font-medium text-gray-900">Under The Kitchen Table</h3>
+              <h2 class="text-sm font-medium text-gray-900">by ekaaryani</h2>
+            </div>
+            <div class="mt-4">
+              <div class="flex items-center justify-between">
+                <h3 class="text-sm text-justify text-gray-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies ornare orci sed suscipit. Maecenas eget luctus ante. Maecenas pellentesque eu diam ut dapibus. Nulla porttitor ac purus vitae condimentum. Mauris consequat metus sit amet sapien congue, a tristique felis ultrices.</h3>
+              </div>
+              <fieldset class="mt-4">
+                  <div class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                  </div>
+              </fieldset>
+            </div>
+            <br>
+            <a href="#" class="text-base font-semibold text-black bg-[#EA9901] py-3 px-8 rounded-full hover:shadow-lg hover:bg-[#BF7D01]">See More</a>
+          </form>
+        </div>
+  
+        <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+          <div>
+            <div class="space-y-6">
+              <h2 class="text-sm font-medium text-gray-900">by ekaaryani</h2>
+              <p class="text-base text-justify text-gray-900">Alkisah ada seorang gadis pendiam bernama Anastasia Mhysa. Atas sikapnya itu, bahkan Anastasia tidak memiliki kawan. Ia hanya bercerita di buku diary berwarna pink yang selalu menemaninya kemana pun. Tanpa diketahui oleh seorang pun, sebenarnya Anastasia juga memiliki buku lainnya. Buku ini sangatlah rahasia karena berisi semua rahasia milik Anastasia. Kehidupan gadis ini tidaklah mudah, ia selalu mendapat perundungan dari Tasya dan Cindi. Namun, akhirnya Bulan datang sebagai peneman untuk Anastasia. Kehadiran Bulan membuat Anastasia lebih ceria daripada sebelumnya. Hingga suatu hari, Anastasia bertemu dengan seorang teman masa kecil bernama Alister. Belum terlalu yakin kalau ia adalah teman masa kecil Anastasia, karena Alister memiliki sifat yang jauh berbeda dengan sifatnya yang dulu. Alister yang ia temui saat ini memiliki sikap yang kasar dan perundung. Dari sini, Anastasia pun memutuskan untuk mencari tahu tentang Alister. Anastasia mengikuti Alister sepulang sekolah. Tanpa disangka, ternyata Alister pergi menuju ke makam ayahnya sambil menangis. Melalui peristiwa ini, Anastasia baru menyadari kalau Alister sebenarnya sosok yang rapuh. Lantas, apakah Alister adalah teman masa kecil Anastasia?</p>
+              <br><br>
+              <a href="#" class="text-base font-semibold text-black bg-[#EA9901] py-3 px-8 rounded-full hover:shadow-lg hover:bg-[#BF7D01]">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>

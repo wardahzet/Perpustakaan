a8dc9032eded4dead31d6edc5f56d882db0d0ac2
betgirs <103440027+betgirs@users.noreply.github.com>
2022-12-05 15:59:03 +0700

    Update searchResult.blade.php

--- resources/views/searchResult.blade.php
+++ resources/views/searchResult.blade.php
@@ -75,12 +75,14 @@
     @endauth
 </div>
 @endif
-
-<div class="title text-2xl font-semibold ml-20 mt-12 mb-4">Search Results</div>
-<div class="content flex py-5 mb-5 mx-20 border-solid border-black">
+<!--============================== SEARCH RESULT ===========================================================-->
+<div class="bg-white">
+  <div class="mx-auto max-w-2xl py-8 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+<div class="title text-2xl font-semibold ml-20 mt-2 mb-4">Search Results</div>
+<div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 xl:gap-x-4">
 
     @foreach ($books as $book)
-      <div class="image-1 mr-8">
+      <div class="image-1 mr-8 hover:scale-110 transition duration-300 ease-in-out">
           <img src="{{$book['cover']}}" alt="">
       </div>
     @endforeach

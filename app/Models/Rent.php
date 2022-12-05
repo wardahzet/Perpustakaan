d1ee5ce0c590d27f4107c1bfb3570a1741950c16
auhppy <auhappycahya11@gmail.com>
2022-12-05 18:43:22 +0700

    Update Rent.php

--- app/Models/Rent.php
+++ app/Models/Rent.php
@@ -9,4 +9,13 @@ class Rent extends Model
 {
     use HasFactory;
     
+    public function books()
+    {
+        return $this->belongsTo(Book::class,'book_isbn','isbn');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_email','email');
+    }
 }

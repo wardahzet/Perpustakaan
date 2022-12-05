b34e8eb0aec1844ad281761f18c1876df67eb8e8
Alifia Putri Budiyanti <101036030+alifiaputri14@users.noreply.github.com>
2022-12-05 16:21:14 +0700

    Update review-peminjaman.blade.php

--- resources/views/review-peminjaman.blade.php
+++ resources/views/review-peminjaman.blade.php
@@ -8,17 +8,68 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-  <div class="relative border-solid border-b-2 border-gray-300 py-2">
-    <div class="container flex items-center px-20">
-      
-        <img src="https://i.ibb.co/Q9vPH3J/Rectangle-119.png" alt="">
-
-        <input type="text" class=" block w-6/12 mx-10 h-10 px-4 py-2 text-base bg-gray-100 bg-clip-padding border-rounded transition ease-in-out focus:text-gray-700 focus:bg-gray-100 focus:border-[#EA9901] focus:outline-none   input-search bg-white-200 border-solid border-2 border-gray-300 rounded-2xl  text-gray-700" placeholder="search" >
-        <a href="#" class="link-1 mx-5 font-semibold text-base  ">Home</a>
-        <a href="#" class="link-2 mx-5 font font-semibold text-base">Rent</a>
-      
+@if (Route::has('login'))
+<div class="relative bg-white">
+    @auth
+    <div class="relative bg-white">
+      <div class="mx-auto">
+        <div class="flex items-center mr-0 ml-0 justify-between border-b-2 border-gray-300 py-6 md:justify-start md:space-x-10">
+            <div class="mb-1 ml-8 flex justify-start lg:w-0 lg:flex-1">
+                <a href="#">
+                <span class="sr-only">E-Library</span>
+                <img class="h-8 w-auto sm:h-10" src="https://i.ibb.co/XYThtt1/Logo-ELibrary.png" alt="">
+                </a>
+            </div>
+        <div class="flex justify-end">
+            
+        <div class="container flex items-center">
+          <input type="text" class="block -ml-32 mx-10 w-96 h-10 px-4 py-2 text-base bg-gray-100 bg-clip-padding border rounded transition ease-in-out focus:text-gray-700 focus:bg-gray-100 focus:border-[#EA9901] focus:outline-none   input-search bg-white-200 border-solid border-2 border-gray-300 rounded-2xl  text-gray-700" placeholder="Search" >
+        </div>
+        </div>
+        <nav class="hidden space-x-10 md:flex">
+            <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
+            <a href="{{ route('rents')}}" class="text-base font-medium text-gray-500 hover:text-gray-900">Rent</a>
+            <a href="{{ route('wishlist')}}" class="text-base font-medium text-gray-500 hover:text-gray-900">Wishlist</a>
+            <a href="{{ route('profile')}}" class="text-base font-medium text-gray-500 hover:text-gray-900">Profile</a>
+        </nav>
+        <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+          <form method="POST" action="{{ route('loogout') }}">
+            @csrf
+            <button type='submit' class="mr-8 inline-flex items-center rounded-md border border-[#EA9901] bg-white px-4 py-2 text-base font-medium text-[#EA9901] shadow-sm hover:bg-gray-300">Logout</button>
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
+@else
+<div class="relative bg-white">
+    <div class="mx-auto">
+        <div class="flex items-center mr-0 ml-0 justify-between border-b-2 border-gray-300 py-6 md:justify-start md:space-x-10">
+            <div class="mb-1 ml-8 flex justify-start lg:w-0 lg:flex-1">
+                <a href="#">
+                <span class="sr-only">E-Library</span>
+                <img class="h-8 w-auto sm:h-10" src="https://i.ibb.co/XYThtt1/Logo-ELibrary.png" alt="">
+                </a>
+            </div>
+        <div class="flex justify-end">
+            
+        <div class="container flex items-center">
+          <input type="text" class="block -ml-80 mx-10 w-96 h-10 px-4 py-2 text-base bg-gray-100 bg-clip-padding border rounded transition ease-in-out focus:text-gray-700 focus:bg-gray-100 focus:border-[#EA9901] focus:outline-none   input-search bg-white-200 border-solid border-2 border-gray-300 rounded-2xl  text-gray-700" placeholder="Search" >
+        </div>
+        </div>
+        <nav class="hidden space-x-10 md:flex">
+            <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
+        </nav>
+        <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0" id="profile">
+          <a href="{{ route('register') }}" class="inline-flex items-center rounded-md border border-[#EA9901] bg-white px-4 py-2 text-base font-medium text-[#EA9901] shadow-sm hover:bg-gray-300">Register</a>
+          <a href="{{ route('login') }}" class="ml-8 mr-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-[#EA9901] -600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-[#BF7D01]">Login</a>
+        </div>
+      </div>
+    </div>
+
+    @endauth
+</div>
+@endif
+
    <div class="profile px-10 my-5 ">
      <div class="container bg bg-yellow-200 px-10 py-5 rounded-lg  ">
        <div class="name flex py-5">
@@ -81,9 +132,10 @@
   </div>
   
     <div class="bg-[#D9D9D9] flex px-16 p-5">
-      <div class="button ml-auto">
-        <button type="submit" class="bg-gray-500 px-16 py-4 rounded-xl  bg-[#EA9901] focus:border-solid focus:bg-white focus:border-[#EA9901] focus:border-2" >Check Out</button>
-      </div>
+    <form method="POST" action="{{ route('loogout') }}" class="flex justify-end  ml-auto">
+            @csrf
+            <button type='submit' class=" inline-flex items-center rounded-md border border-[#EA9901] bg-[#EA9901] px-5 py-2 text-base font-medium text-white shadow-sm focus:bg-white focus:text-[#EA9901] ml-auto">Check Out</button>
+          </form>
     </div>
 </body>
 </html>

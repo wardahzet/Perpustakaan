01ef98b0015fb1f29bfcc4cb84eacdd0c11f1e23
Siti Waheeda Najihah <sitiwaheeda14@student.ub.ac.id>
2022-12-05 16:09:33 +0700

    Update detailRent.blade.php

--- resources/views/detailRent.blade.php
+++ resources/views/detailRent.blade.php
@@ -53,20 +53,20 @@
       <div class="content">
         <div class="container bg-[rgb(255,250,239)] flex px-10 rounded-lg ">
         <div class="checkboks w-1/12 py-20 flex justify-center">
-          <form action="" method="">
+          <form action="" method="post">
             <label for=""></label>
             <input type="checkbox" id="" value="">
           </form>
         </div>
           <div class="nama-product  w-3/12 py-5 flex">
           <div class="image  mr-5">
-            <img src="https://i.ibb.co/M7r1MVM/Rectangle-115.png" alt="">
+            <img src="{{$wishlist->books['cover']}}" alt="">
           </div>
-          <div class="name-title  flex items-center font-semibold">Under The Kitchen Table - Dessy Miladeana</div>
+          <div class="name-title  flex items-center font-semibold">{{$wishlist->books['title']}}</div>
           </div>
           <div class="ISBN   w-2/12 py-5 flex items-center justify-center">{{$wishlist->books['isbn']}}</div>
-          <div class="Penulis   w-2/12 py-5 flex items-center justify-center">Dessy Miladeana</div>
-          <div class="Penerbit  w-2/12 py-5 flex items-center justify-center">Erlangga</div>
+          <div class="Penulis   w-2/12 py-5 flex items-center justify-center">{{$wishlist->books['author']}}</div>
+          <div class="Penerbit  w-2/12 py-5 flex items-center justify-center">{{$wishlist->books['publisher']}}</div>
           <div class="Kategories  w-1/12 py-5 flex justify-center items-center" >Fiction</div>
           <div class="button w-1/12 py-20 justify-center items-center">
             <button type="submit" class="delete bg-[rgb(234,153,1)]-500 px-6 py-2 rounded-xl  bg-[#EA9901] focus:border-solid focus:bg-white focus:border-[#EA9901] focus:border-2" >Delete</button>
@@ -75,22 +75,21 @@
       <div class="content ">
         <div class="container bg-[rgb(255,250,239)] flex px-10 rounded-lg">
         <div class="checkboks w-1/12 py-20 flex justify-center">
-        <form action="" method="">
+        <form action="" method="post">
           <label for=""></label>
           <input type="checkbox" id="" value="">
         </form>
       </div>
           <div class="nama-product  w-3/12 py-5  flex">
           <div class="image mr-5">
-            <img src="https://i.ibb.co/h2zb9sd/123321.png" alt="">
+            <img src="{{$wishlist->books['cover']}}" alt="">
           </div>
-          <div class="name-title   flex items-center font-semibold">Ensiklopedia Sains (Dilengkapi 1000 tautan internet)</div>
+          <div class="name-title   flex items-center font-semibold">{{$wishlist->books['title']}}</div>
           </div>
-          <div class="ISBN   w-2/12 py-5 flex items-center justify-center">9786022497301</div>
-          <div class="Penulis   w-2/12 py-5 flex items-center justify-center">Usborne</div>
-          <div class="Penerbit   w-2/12 py-5 flex items-center justify-center">Bhuana Ilmu <br>Populer
-          </div>
-          <div class="Kategories  w-1/12 py-5 flex justify-center items-center" >Encyclopedia</div>
+          <div class="ISBN   w-2/12 py-5 flex items-center justify-center">{{$wishlist->books['isbn']}}</div>
+          <div class="Penulis   w-2/12 py-5 flex items-center justify-center">{{$wishlist->books['author']}}</div>
+          <div class="Penerbit   w-2/12 py-5 flex items-center justify-center">{{$wishlist->books['publisher']}}</div>
+          <div class="Kategories  w-1/12 py-5 flex justify-center items-center" >{{$wishlist->books['publisher']}}</div>
           <div class="button w-1/12 py-20 justify-center items-center">
             <button type="submit" class="delete bg-[rgb(234,153,1)]-500 px-6 py-2 rounded-xl  bg-[#EA9901] focus:border-solid focus:bg-white focus:border-[#EA9901] focus:border-2" >Delete</button>
         </div>
